5b018410f10dc0d1a1b275344b32b701c49ebd71
carloskauan <carloskauan190@gmail.com>
2022-04-20 09:30:33 -0300

    resolution error - dotenv !

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,15 @@ const express = require('express')
 const mongoose = require ('mongoose') 
 const routes = require('./routes') // import
 
+// dotenv
+require('dotenv').config()
+
 // Criando app
 const app = express() 
-// dotenv
-require('dotenv').config
+
+app.use(express.json()) // Utilizar formato Json
+
+app.use(routes) // usar routes
 
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
@@ -18,9 +23,6 @@ mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@omnistack.p0kcr.mongod
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-
-app.use(express.json()) // Utilizar formato Json
-app.use(routes) // usar routes
 
 // rodando server 
 app.listen(3333, (req, res) => console.log('server running 🤖'));
